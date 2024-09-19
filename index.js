3e9dcee2453ab542bd8992d6479f9323e63fcad2
devakhil-kumar <akhil.cyberbells.com>
2024-09-19 18:00:06 +0530

    changes

--- index.js
+++ index.js
@@ -17,7 +17,13 @@ import userRoutes
  import notificationRoutes from './routes/notificationRoutes.js';
 const app= express();
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://95.216.209.46:3000');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // app.use(bodyParser.json());
